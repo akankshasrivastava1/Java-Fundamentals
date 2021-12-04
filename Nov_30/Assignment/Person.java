e5e1fadb6c8c73486d20db484d55e7179f2adc9c
Akanksha Srivastava <47323489+akankshasrivastava1@users.noreply.github.com>
2021-12-05 03:16:55 +0530

    Update Person.java

--- Nov_30/Assignment/Person.java
+++ Nov_30/Assignment/Person.java
@@ -57,6 +57,9 @@ public class Person {
    // main method for testing
    public static void main(String[] args)
    {
+	   Person p = new Person("Sana");
+           System.out.println(p);
+          
 	   Person p1  = new Person();
            System.out.println(p1.getName());
 	   System.out.println(p1.getage());  
