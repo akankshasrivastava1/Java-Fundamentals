9586091507bec908892189e49e1f8032f1afaf75
Akanksha Srivastava <47323489+akankshasrivastava1@users.noreply.github.com>
2021-12-05 16:15:19 +0530

    Update Test1.java

--- Dec_4/Assignment/Test1.java
+++ Dec_4/Assignment/Test1.java
@@ -1,12 +1,12 @@
 package Dec_4.Assignment;
-
+//Write a method that overloads the talk method by taking in a name and printing “Hello” with that name.
 public class Test1 {
     public static void talk()
     {
           System.out.println("hello there!");
     }
 
-    public static void talk(String name){// FINISH THE METHOD HERE //
+    public static void talk(String name){
         System.out.println("hello "+name);
     }
 
